fae049f3cbd1e4ca63bf21b8bd3aaabde7c86b9b
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2023-02-25 01:05:40 -0500

    8 files modified

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tailwind Devdudes Astro Theme';
+export const SITE_DESCRIPTION = 'Check out my Astro template dudes!';
